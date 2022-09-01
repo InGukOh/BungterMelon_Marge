41c787e1670270231081ab7016c23d1b0de6d384
InGukOh <87802273+InGukOh@users.noreply.github.com>
2022-09-01 22:16:02 +0900

    iframe 과 부모창 통신중

--- src/project.js
+++ src/project.js
@@ -2,6 +2,12 @@
  * 选分弹窗
  * @type {number}
  */
+window.onload = function (){
+  console.log("온로드 실행 테스트중");
+
+  window.parent.postMessage({Data : 'testtesttesttesttest!!!!!!!!!!!'}, '*');
+
+}
 var score_in_Page = parent.score;
 if(score_in_Page.value == 0){
   alert("0입니다!!!!")
